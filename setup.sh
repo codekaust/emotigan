6878666f46b87935d88d073ac2e845aaac89f367
kaaira <kaaira@root1-HP-Z8-G4-Workstation>
2020-03-24 00:52:40 +0530

    minor

--- setup.sh
+++ setup.sh
@@ -4,7 +4,12 @@ cd utils/
 
 wget http://nlp.stanford.edu/data/glove.6B.zip
 unzip glove.6B.zip
+rm glove.6B.100d.txt glove.6B.zip glove.6B.50d.txt glove.6B.200d.txt
+
+cd ..
 
 # create required directories
 mkdir -p images/snapshot
 mkdir saved_model/
+mkdir -p dataset/edited
+mkdir -p dataset/original
